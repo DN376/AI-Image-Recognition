8b612306ffc9d02f72d8c16dc3892052659065fc
Dave <dave030706@gmail.com>
2022-05-02 20:59:17 -0400

    first commit

--- font_experimental.py
+++ font_experimental.py
@@ -1,29 +1,4 @@
-import PIL
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 
-def getSize(txt, font):
-    testImg = Image.new('RGB', (1, 1))
-    testDraw = ImageDraw.Draw(testImg)
-    return testDraw.textsize(txt, font)
-
-if __name__ == '__main__':
-
-    fontname = "arial.ttf"
-    fontsize = 11   
-    text = "example@gmail.com"
-    
-    colorText = "black"
-    colorOutline = "red"
-    colorBackground = "white"
-
-
-    font = ImageFont.truetype(fontname, fontsize)
-    width, height = getSize(text, font)
-    img = Image.new('RGB', (width+4, height+4), colorBackground)
-    d = ImageDraw.Draw(img)
-    d.text((2, height/2), text, fill=colorText, font=font)
-    d.rectangle((0, 0, width+3, height+3), outline=colorOutline)
-    
-    img.save("C:\Users\Dave0\Code\Computer_Sci_Abelard\src\Independent Project\images\image.png")
+def StringReverser(string):
+    return string[::-1]
+print(StringReverser("soidnehf"))
